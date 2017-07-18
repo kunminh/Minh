0f78844f4179051ffdf0a6d52b77f2e77d6c4309
kunminh <kunminh@gmail.com>
2017-07-18 23:00:06 +0700

    vẽ hình chữ nhật

---
+++ ve tam giac/ve hinh chu nhat.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+ main() {
+   int a, b, i, j ;	
+   printf("nhap do dai hai canh cua hinh chu nhat");
+   scanf("%d%d",&a,&b); 
+   for(i=1;i<=a;i++){
+	  for(j=1;j<=b;j++)
+
+
+	      printf(" * ");
+	  printf("\n\n");  
+   getch();
+}
+}
