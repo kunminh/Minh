a13542576899dfe6e2ff0558e5cea2411317199a
kunminh <kunminh@gmail.com>
2017-07-18 23:30:57 +0700

    vẽ hình chữ nhật có hai đường chéo

---
+++ ve tam giac/ve hinh chu nhat 1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main() {
+   int a, b, i, j ;	
+   printf("nhap do dai hai canh cua hinh chu nhat");
+   scanf("%d%d",&a,&b); 
+   for(i=1;i<=a;i++){
+          for(j=1;j<=b;j++)
+        
+	      printf(" * ");
+	  printf("\n\n");  
+   
+}
+}
