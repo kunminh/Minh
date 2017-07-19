8b5562d2004a4316717ee021216aeee9f64272f9
kunminh <kunminh@gmail.com>
2017-07-19 09:40:23 +0700

    vẽ hình vuông

--- ve tam giac/ve hinh vuong.cpp
+++ ve tam giac/ve hinh vuong.cpp
@@ -1,14 +1,12 @@
 #include <stdio.h>
 int  main() {
    int a, i, j ;	
-   printf("nhap do dai hai canh cua hinh chu nhat");
+   printf("nhap do dai canh cua hinh vuong");
    scanf("%d",&a); 
    for(i=1;i<=a;i++){
 	  for(j=1;j<=a;j++)
          if(i==1||j==1||i==a||j==a||(i+j)%2==0)
          printf(" * ");
-		 /*if((i+j)%2==0)
-	      printf(" * ");*/
 	     else
 		printf("   ");  
 	  printf("\n\n");  
