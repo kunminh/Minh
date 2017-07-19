db1a3e9e2db618c7429339779c2d2d8abdb0c8b4
kunminh <kunminh@gmail.com>
2017-07-19 09:24:06 +0700

    vẽ hình vuồng rỗng

---
+++ ve tam giac/ve hinh vuong rong.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int  main() {
+   int a, b, i, j ;	
+   printf("nhap do dai canh cua hinh vuong");
+   scanf("%d",&a); 
+   for(i=1;i<=a;i++){
+	  for(j=1;j<=a;j++)
+         if(i==1||j==1||i==a||j==a)
+         printf(" * ");
+	     else
+		printf("   ");  
+	  printf("\n\n");  
+   
+}
+}
