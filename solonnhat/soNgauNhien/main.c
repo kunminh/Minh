4d41427f1a0984f668b16bc958fd6ff9e37d9c79
kunminh <kunminh@gmail.com>
2017-08-01 17:32:58 +0700

    Số ngẫu nhiên

--- solonnhat/soNgauNhien/main.c
+++ solonnhat/soNgauNhien/main.c
@@ -1,18 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
-int main()
+void xepBatKi (int ngaunhien [1000], int N)
 {
-   int i,k,N;
-   int ngaunhien[1000];
-   printf("Nhap so phan tu cua mang:");
-   scanf("%d",&N);
-   for (i=0;i<N;i++)
-    {
-	 	ngaunhien[i] = (rand() %1000);
-    }
-
-
     for(int k = 0; k < N-1; k++)
     {
         int imax = ngaunhien[k];
@@ -26,11 +17,42 @@ int main()
             }
         }
     }
+}
+int main()
+{
+    //nhap so phan tu cua mang
+    int i,N;
+    int ngaunhien[10000];
+    printf("Nhap so phan tu cua mang:");
+    scanf("%d",&N);
+    for (i=0; i<N; i++)
+    {
+        ngaunhien[i] = (rand() %1000);
+    }
 
-    for(i=0;i<N;i++)
+
+    struct timeval t1, t2;
+    double elapsedTime;
+
+    // start timer
+    gettimeofday(&t1, NULL);
+    //sap xep mang
+    xepBatKi(ngaunhien,N);
+
+    // stop timer
+    gettimeofday(&t2, NULL);
+
+    // compute and print the elapsed time in millisec
+    elapsedTime = (t2.tv_sec - t1.tv_sec) * 1000.0;      // sec to ms
+    elapsedTime += (t2.tv_usec - t1.tv_usec) / 1000.0;   // us to ms
+
+    //in ket qua
+    for(i=0; i<N; i++)
     {
         printf("%d\n",ngaunhien[i]);
     }
 
+    printf("Thoi gian chay %f", elapsedTime );
 
+    return 0;
 }
